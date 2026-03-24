b66fe85e3f905e0f8afcd2d82b364a954aea4107
Manisha Kanojia <manishakanojia95@gmail.com>
2026-03-24 14:23:33 +0530

    Update generate_project.py

--- generate_project.py
+++ generate_project.py
@@ -6,9 +6,7 @@ folder = f"project-{today}"
 
 os.makedirs(folder, exist_ok=True)
 
-content = f"# Project {today}\n\nThis is your auto-generated project."
-
 with open(f"{folder}/README.md", "w") as f:
-    f.write(content)
+    f.write(f"# Project {today}\n\nAuto-generated project working ✅")
 
 print("Project created successfully")
